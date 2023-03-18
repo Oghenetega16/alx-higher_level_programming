50b78a0b2c4eefeb6766225529d9971dd33358a8
oghenetega16 <sukuruoghenetega@gmail.com>
2023-03-18 23:50:25 +0100

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""
-lists all State objects from a database
-"""
-
-import sqlalchemy
+'''Prints all State objects in a database.
+'''
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 from model_state import Base, State
 
-if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                                argv[2],
-                                                                                                                                                    argv[3]))
-            Base.metadata.create_all(eng)
-                Session = sessionmaker(bind=eng)
-                    session = Session()
-                        for state in session.query(State).order_by(State.id):
-                                    print("{}: {}".format(state.id, state.name))
-                                        session.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).all()
+        for res in result:
+            print('{}: {}'.format(res.id, res.name))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""
-list the first State object from a database
-"""
-
-import sqlalchemy
+'''Prints the first State object in a database.
+'''
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 from model_state import Base, State
 
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state is not None:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
-        print("Nothing")
-    session.close()
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).first()
+        if result is not None:
+            print('{}: {}'.format(result.id, result.name))
+        else:
+            print('Nothing')
